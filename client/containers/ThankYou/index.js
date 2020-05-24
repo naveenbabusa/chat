86656988c9dc44de2e77143fbba0d6d123d5629e
Dharanidhar Reddy <dharanidhar01.04@gmail.com>
2020-05-24 20:45:13 +0530

    Added README.md

--- client/containers/ThankYou/index.js
+++ client/containers/ThankYou/index.js
@@ -1,29 +1,12 @@
 import "./main.css";
 
-import { Button } from "react-bootstrap";
 import React from "react";
 
 function ThankYouPage() {
-	const redirect = () => {
-		window.open("https://forms.gle/jKRaYofjxLePWhKQA", "_self");
-	};
-
 	return (
 		<div className="thank-you-page">
 			<div>
 				<h3 className="thank-you-text">Thank you</h3>
-			</div>
-			<div>
-				<Button
-					variant="warning"
-					size="lg"
-					className="join-button"
-					type="submit"
-					onClick={redirect}
-					block
-				>
-					Provide Feedback
-				</Button>
 			</div>
 		</div>
 	);
